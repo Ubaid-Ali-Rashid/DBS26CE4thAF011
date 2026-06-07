ea5d283bdd6cd4cfd3cb257de106ce71451c5163
Ubaid Ali <ubaidrashid256@gmail.com>
2026-06-07 22:30:55 +0500

    Add files via upload

---
+++ Hotel Management System/Data/DbConnection.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace HotelManagementSystem.Data
+{
+    public class DbConnection
+    {
+        private readonly string _connectionString;
+
+        public DbConnection()
+        {
+            _connectionString = "Server=localhost;Database=HotelBookingSystem;Uid=root;Pwd=;";
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(_connectionString);
+        }
+    }
+}
